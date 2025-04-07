96051b47e335a385b7d135f20f8c8b5a48d04195
Owen <juanmig134187@gmail.com>
2025-04-07 22:04:07 +0800

    adding_line_to_pseudocode_for_def_function

--- quiz_creator_.py
+++ quiz_creator_.py
@@ -1,9 +1,10 @@
 # pseudocode
-# use while loop for first loop
-    # ask for user input
-    # ask for question
-    # ask for choices
-    # ask for the correct answer
+# use def for questionare
+    # use while loop for first loop
+        # ask for user input
+        # ask for question
+        # ask for choices
+        # ask for the correct answer
 # use def to save the data into file
     # use file.write function to store
     #
