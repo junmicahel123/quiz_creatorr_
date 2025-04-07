1a5406a61970f78419d9ee30257183371846b36b
Owen <juanmig134187@gmail.com>
2025-04-07 22:02:33 +0800

    pseudocode_

---
+++ quiz_creator_.py
@@ -0,0 +1,11 @@
+# pseudocode
+# use while loop for first loop
+    # ask for user input
+    # ask for question
+    # ask for choices
+    # ask for the correct answer
+# use def to save the data into file
+    # use file.write function to store
+    #
+
+
