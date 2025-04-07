619b88676e8b109c65dbd35987107191ae73d917
Owen <juanmig134187@gmail.com>
2025-04-07 22:22:08 +0800

    first_def_and_while_loop_for_questions_choices_correct_answers_also_added_a_number_of_items_counter_and_chr_funct_for_a_b_c_d_in_for_loop_

--- quiz_creator_.py
+++ quiz_creator_.py
@@ -10,3 +10,30 @@
     #
 
 
+def questionare():
+    number_of_items = 0
+    questions = []
+    while True:
+        question = input("Please emter a question::  ")
+        choices = []
+        for i in range(4):
+            choice = input(f"Enter the choices for {97 + i}::")
+        
+            choices.append(choice)
+        correct_answer = input("What is the correct answer? (A, B, C,)").strip().lower()
+
+        questions.append({
+            'Question': question,
+            'Choices': choices,
+            'Correct Answer': correct_answer 
+        })
+        number_of_items += 1
+        continueum = input("Do you want to add another question?: (y/n)").strip().lower()
+        if continueum != 'y':
+            break
+
+
+
+
+
+            
